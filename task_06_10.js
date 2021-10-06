3f828d1d4b169bd3bb9430a730f928826883664c
Alexander <zionya@ukr.net>
2021-10-06 15:29:22 +0300

    fixed style

--- task_06_10.js
+++ task_06_10.js
@@ -1,87 +1,96 @@
 "use strict"
 
-function getFibonacci (num){
+function getFibonacci(num) {
 
     let first = 0;
     let second = 1;
-    let fibonacci = [first, second];
+    let fibonacci = [];
 
-    if (num < 1) return 0;
-
-    if (num == 1){
-
+    if (num < 1) {
         return fibonacci;
+    } 
 
+    fibonacci.push(first);
+
+    if (num < 2) {
+        return fibonacci;
     }
 
-    for (let i = 1; i < num; i++){
+    fibonacci.push(second);
+
+    for (let i = 2; i < num; i++) {
+
         let nextNumber = first + second;
         fibonacci.push(nextNumber);
         first = second;
         second = nextNumber;
+
     }
 
     return fibonacci;
 
 }
-//console.log (getFibonacci(5));
+//console.log(getFibonacci(3));
 
-function getAreaRectangle(length, width){
+let rectangle = {
 
-    return length*width;
+    height: 5,
+    width: 6,
+    square() {
+        return this.height * this.width;    
+    },
+    perimeter() {
+        return 2 * this.height + 2 * this.width;
+    },
 
 }
 
-//console.log(getAreaRectangle(5, 6));
+//console.log(rectangle.square());
+//console.log(rectangle.perimeter());
 
-function getPerimeterRectangle (length, width){
+let triangle = {
 
-    return 2*length + 2*width;
+    side1: 3,
+    side2: 4,
+    side3: 5,
+    square() {
 
-}
-
-// console.log(getPerimeterRectangle(5, 6));
-
-function getAreaTriangle (side1, side2, side3){
-
-    let semiPerimeter = (side1 + side2 + side3)/2;
+        let semiPerimeter = (this.side1 + this.side2 + this.side3) / 2;
     
-    return Math.sqrt(semiPerimeter*(semiPerimeter - side1)*(semiPerimeter - side2)*(semiPerimeter - side3)); 
+        return Math.sqrt(semiPerimeter * (semiPerimeter - this.side1) * (semiPerimeter - this.side2) * (semiPerimeter - this.side3)); 
+  
+    },
+    perimeter() {
+        return this.side1 + this.side2 + this.side3
+    },
 
 }
 
-//console.log(getAreaTriangle (3, 4, 5))
+//console.log(triangle.square())
+//console.log(triangle.perimeter())
 
-function getPerimeterTriangle (side1, side2, side3){
+let circle = {
 
-    return side1 + side2 + side3;
-
-}
-
-//console.log(getPerimeterTriangle(2,3,4));
-
-function getAreaCircle (radius){
-
-    return Math.PI*radius*radius; 
-
-}
-
-//console.log(getAreaCircle(4))
-
-function getLengthCircle (radius){
-
-    return 2*Math.PI*radius;
+    radius: 4,
+    square() {
+        return Math.PI * this.radius * this.radius;
+    },
+    length() {
+        return 2 * Math.PI * this.radius;
+    },
 
 }
-//console.log(getLengthCircle(10))
 
-function getFactorial(num){
+//console.log(circle.square());
+//console.log(circle.length())
+
+function getFactorial(num) {
     
     let factorial = 1;
 
-    for(let i = 0; i < num; i++){
+    for (let i = 0; i < num; i++) {
 
-        factorial = factorial*(i + 1);
+        factorial = factorial * (i + 1);
 
     }
 
@@ -89,16 +98,17 @@ function getFactorial(num){
 
 }
 
-//console.log(getFactorial(2));
+//console.log(getFactorial(5));
 
-function getSumMultipleOf2(arr){
+function getSumMultipleOf2(arr) {
 
     let sum = 0;
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
-        if(arr[i]%2 === 0 ) sum += arr[i];
-
+        if (arr[i] % 2 === 0 ) {
+            sum += arr[i];
+        }
     }
 
     return sum;
@@ -107,13 +117,15 @@ function getSumMultipleOf2(arr){
 
 //console.log (getSumMultipleOf2([-2, 4, 6, 7, 8, 9, 10, 12, 20, 21]));
 
-function getSumMultipleOf3(arr){
+function getSumMultipleOf3(arr) {
 
     let sum = 0;
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
-        if(arr[i]%3 === 0 ) sum += arr[i];
+        if (arr[i] % 3 === 0 ) {
+            sum += arr[i];
+        } 
 
     }
 
@@ -122,13 +134,15 @@ function getSumMultipleOf3(arr){
 
 //console.log (getSumMultipleOf3([2, 4, 6, 7, 8, 9, 10, 12, 20, 21]));
 
-function getSumPositive(arr){
+function getSumPositive(arr) {
     
     let sum = 0;
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
-        if(arr[i] > 0 ) sum += arr[i];
+        if (arr[i] > 0 ) {
+            sum += arr[i];
+        }
 
     }
 
@@ -139,13 +153,15 @@ function getSumPositive(arr){
 //console.log (getSumPositive([-2, 4, 6, -7, 8, -9, 10, 12, -20, 21]));
 
 
-function getSumOdd(arr){
+function getSumOdd(arr) {
     
     let sum = 0;
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
-        if(Math.abs(arr[i]%2) > 0 ) sum += arr[i];
+        if(Math.abs(arr[i] % 2) > 0 ) {
+            sum += arr[i];
+        } 
 
     }
 
@@ -153,15 +169,17 @@ function getSumOdd(arr){
 
 }
 
-//console.log (getSumOdd([-2, 4, 6, -7, 8, -9, 10, 12, -20, 21]));
+//console.log (getSumOdd([2, 4, 6, 7, 8, 9, 10, 12, 20, 21]));
 
-function getCountedElemZero(arr){
+function getCountedZeroElem(arr) {
 
     let counter = 0;
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
-        if(arr[i] === 0 ) counter++;
+        if(arr[i] === 0 ) {
+            counter++;
+        } 
 
     }
 
@@ -169,16 +187,18 @@ function getCountedElemZero(arr){
 
 }
 
-//console.log (getCountedElemZero([-2, 0, 0, 6, 0, 8, 0, 10, 0, -20, 21]));
+//console.log (getCountedZeroElem([-2, 0, 0, 6, 0, 8, 0, 10, 0, -20, 21]));
 
 
-function getCountedElemNegative(arr){
+function getCountedNegativeElem(arr) {
 
     let counter = 0;
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
-        if(arr[i] < 0 ) counter++;
+        if (arr[i] < 0 ) {
+            counter++;
+        } 
 
     }
 
@@ -186,15 +206,17 @@ function getCountedElemNegative(arr){
 
 }
 
-//console.log (getCountedElemNegative([-2, 0, 0, 6, 0, 8, 0, 10, 0, -20, 21]));
+//console.log (getCountedNegativeElem([-2, 0, 0, 6, 0, 8, 0, 10, 0, -20, 21]));
 
-function getCountedElemPositive(arr){
+function getCountedPositiveElem(arr) {
 
     let counter = 0;
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
-        if(arr[i] > 0 ) counter++;
+        if(arr[i] > 0 ) {
+            counter++;
+        } 
 
     }
 
@@ -202,38 +224,42 @@ function getCountedElemPositive(arr){
 
 }
 
-//console.log (getCountedElemPositive([-2, 0, 0, 6, 0, 8, 0, 10, 0, -20, 21, 4]));
+//console.log (getCountedPositiveElem([-2, 0, 0, 6, 0, 8, 0, 10, 0, -20, 21, 4]));
 
 
-function getCountedElemPrame(arr){
+function getCountedPrameElem(arr) {
 
     let counter = 0;
 
-    function isPrimeNumber(num){
+    function isPrimeNumber(num) {
 
         let counter = 0;
 
-        if (num < 2){
-
+        if (num < 2) {
             return false;
-            
         }
 
-        for (let i = 1; i < num; i++){
+        for (let i = 1; i < num; i++) {
 
-            if(num%i === 0) counter++;
+            if (num % i === 0) {
+                counter++;
+            }
 
         }
 
-        if(counter > 1) return false;
+        if (counter > 1) {
+            return false;
+        } 
 
         return true;
 
     }
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
-        if(isPrimeNumber(arr[i])) counter++;
+        if ( isPrimeNumber(arr[i]) ) {
+            counter++;
+        } 
 
     }
 
@@ -241,4 +267,4 @@ function getCountedElemPrame(arr){
 
 }
 
-//console.log (getCountedElemPrame([2, 11, 0, 3, 0, 8, 0, 7, 0, -5, 21, 4]));
+//console.log (getCountedPrameElem([2, 11, 0, 3, 0, 8, 0, 7, 0, 5, 21, 4]));
